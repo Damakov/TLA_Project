1d165cd4108f751f242054097cd3772527b29f62
yaboiiii23 <48087333+yaboiiii23@users.noreply.github.com>
2023-01-09 09:44:22 +0100

    Update TP4.java
    Modification de la class pour pouvoir faire les analyses

--- projet-tla/src/main/java/analyses/TP4.java
+++ projet-tla/src/main/java/analyses/TP4.java
@@ -1,32 +1,30 @@
 package analyses;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class TP4 {
 
 	public static void main(String[] args) {
-			LireFichierTxt a = new LireFichierTxt();
 			String path = "C://Users/Sewraj/Documents/tlaTest.txt" ;
-			ArrayList<ArrayList<String>> arr = a.lireFichier(path);
-			String entree = concatToString(arr);
-			System.out.println(entree);
+			ArrayList<ArrayList<String>> arr = LireFichierTxt.lireFichier(path);
+			try {
+				String entree = LireFichierTxt.readFileAsString(path);
+				System.out.println(entree);
+				testAnalyseLexicale(entree);
+				testAnalyseSyntaxique(entree);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
-
-	}
+	 }
 
 	/*
 	effectue l'analyse lexicale de la chaine entree,
 	affiche la liste des tokens reconnus
 	 */
-	public static String concatToString(ArrayList<ArrayList<String>> allElements) {
-		ArrayList<String> concat = new ArrayList<String>();
-		concat.addAll(allElements.get(0));
-		concat.addAll(allElements.get(1));
-		concat.addAll(allElements.get(2));
-		concat.addAll(allElements.get(3));
-		
-		return concat.toString();
-	      }
+
 	
 	
 	private static void testAnalyseLexicale(String entree) {
@@ -50,7 +48,7 @@ public class TP4 {
 		try {
 			List<Token> tokens = new AnalyseLexicale().analyse(entree);
 			String res = new AnalyseSyntaxique().analyse(tokens);
-			System.out.println("La valeur de l'expression " + entree + " est " + res);
+			System.out.println("L'analyse lexical et synaxique a ete fait");
 		} catch (Exception e) {
 			e.printStackTrace(System.out);
 		}
