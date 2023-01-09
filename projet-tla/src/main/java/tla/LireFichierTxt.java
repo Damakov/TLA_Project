a74a1c18f019750065abe70187a3bc225f99b43a
yaboiiii23 <48087333+yaboiiii23@users.noreply.github.com>
2023-01-09 09:33:14 +0100

    Update LireFichierTxt.java
    Ajout d'une methode qui met tout le fichier txt dans un string pour que ca soit plus simple pour les analyses

--- projet-tla/src/main/java/tla/LireFichierTxt.java
+++ projet-tla/src/main/java/tla/LireFichierTxt.java
@@ -128,15 +128,13 @@ public class LireFichierTxt {
 		
 		return allElements;		
 }
+public static String readFileAsString(String fileName)throws Exception
+	  {
+	    String data = "";
+	    data = new String(Files.readAllBytes(Paths.get(fileName)));
+	    return data;
+}
 
-
-	public static void main(String[] args) {
-		
-		String path = "C://Users/marti/Desktop/tla.txt" ;
-		lireFichier(path);
-		// Afficher tout l'arraylist, chaque éléments est en [0],[1],[2],[3]
- 		//System.out.println(lireFichier(path));
-		
-	}	
+	
 
 }
